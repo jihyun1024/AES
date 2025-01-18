11afffd094ca80d49f511e076709d85a425d15e8
jihyun <gkang0702@gmail.com>
2025-01-18 21:36:49 +0900

    Update AES32.h

--- Operation Mode/ECB & CBC/AES32.h
+++ Operation Mode/ECB & CBC/AES32.h
@@ -3,10 +3,11 @@
 typedef unsigned char byte;
 typedef unsigned int u32;
 
-// 바이트 배열 b[4] --> u32 정수로
+// byte array b[4] --> u32 int
 #define GETU32(b) ((u32)(b)[0] << 24) ^ ((u32)(b)[1] << 16) \
             ^ ((u32)(b)[2] << 8) ^ ((u32)(b)[3]);
-// u32 정수 --> 바이트 배열 b[4]
+
+// u32 int --> byte array b[4]
 #define PUTU32(b, x) { \
     (b)[0] = (byte) ((x)>>24); \
     (b)[1] = (byte)((x) >> 16); \
@@ -19,7 +20,6 @@ void AES32_Enc_KeySchedule(byte k[16], u32 rk[11][4]);
 void AES32_Dec_KeySchedule(byte k[16], u32 rk[11][4]);
 void byte2state(byte b[16], u32 st[4]);
 void state2byte(u32 st[4], byte b[16]);
-void AES8_KeySchedule(byte k[16], byte rk[11][16]);
 void AES32_EncRound(u32 st[4], u32 rk[4]);
 void AES32_EqDecRound(u32 st[4], u32 rk[4]);
 void AES32_Encrypt(byte pt[16], u32 rk[11][4], byte ct[16]);
